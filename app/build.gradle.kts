181d39b065b66df976220053a1f496bf25962ab4
p2793934 <p2793934@my365.dmu.ac.uk>
2025-11-04 12:15:00 +0000

    Attempted to connect FireBase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,5 +47,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
+    implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
 }
